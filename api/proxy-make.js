b187f8f7c2327dd9906b821fff8c29880f9d98a1
propleaderOFFICIAL <propleaderofficial@gmail.com>
2025-11-14 03:10:37 +0100

    Update proxy-make.js

--- api/proxy-make.js
+++ api/proxy-make.js
@@ -54,6 +54,7 @@ export default async function handler(req, res) {
     const allowedWebhooks = [
       // LEAD su n8n
       'https://digitalsolutions.app.n8n.cloud/webhook/875aa7f1-389c-4182-b241-5526dc271962',
+      'https://digitalsolutions.app.n8n.cloud/webhook-test/875aa7f1-389c-4182-b241-5526dc271962',
     
       // TRACKING ancora su Make
       'https://hook.eu2.make.com/6c532l9lbrpji3mjm6decgduwt8hbvqw'
